73e879af87012352f08c5449ec668a8c165a7815
Angela Mitchell <angela.mitchell@vmlyr.com>
2021-05-31 22:30:15 -0500

    add calculateAge method; rename image property to imageUrl

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = Vue.createApp({
         return {
             name: 'Angela',
             age: 32,
-            image: 'https://cdn.pixabay.com/photo/2015/03/26/09/47/sky-690293__340.jpg'
+            imageUrl: 'https://cdn.pixabay.com/photo/2015/03/26/09/47/sky-690293__340.jpg'
         } 
     },
     methods: {
+        calculateAge() {
+            return this.age + 5;
+        },
         outputRandomNum() {
             const randomNum = Math.random();
             return randomNum;
